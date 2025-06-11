b6919cdfaa63a0b2fcb7b4ec5984276806dfe608
Aaron <aer35@users.noreply.github.com>
2025-06-11 15:39:38 -0400

    modify the deploy-commands to remove old commands first. Added new ENV variable

--- src/config.ts
+++ src/config.ts
@@ -7,6 +7,12 @@ export const DISCORD_TOKEN = process.env.DISCORD_TOKEN as string;
 export const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID as string;
 export const DISCORD_GUILD_ID = process.env.DISCORD_GUILD_ID as string;
 
+export const SUBREDDIT_NAME = process.env.SUBREDDIT_NAME as string;
+
 if (!DISCORD_TOKEN || !DISCORD_CLIENT_ID || !DISCORD_GUILD_ID) {
-    throw new Error("Missing environment variables");
+    throw new Error("Missing required discord tokens");
+}
+
+if (!SUBREDDIT_NAME) {
+    throw new Error("Missing sub reddit name");
 }
